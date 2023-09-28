d873a74f853d7c3128c7ae1a14a07f820c9bc81a
Diana <116961299+dianalima184@users.noreply.github.com>
2023-09-27 23:52:13 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+
+import {useFormik} from 'formik';
+
+
+function App(){
+const formik = useFormik({
+  initialValues:{
+  name: '',
+  email:'',
+  password:''
+},
+onSubmit: values =>{
+  console.log('form:', values);
+}
+})
+
+
+  return (
+  <div>
+ <form onSubmit={formik.handleSubmit}>
+<div>Name</div>
+<input name= "name" type="text" onChange={formik.handleChange}
+value={formik.values.name}/> 
+<div>Email</div>
+<input name="email" type="text" onChange={formik.handleChange}
+value= {formik.values.email}/>
+<div>Password</div>
+<input name="password" type="text" onChange={formik.handleChange}
+value={formik.values.password}/>
+  <button type="submit">Submit</button>
+  </form>
+
+
+      </div> 
+  );
+}
+
+export default App;
